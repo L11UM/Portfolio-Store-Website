b407c5ad62ddb26d941a63269cba7e0753d7a9a6
LiiUM <liambt20@gmail.com>
2023-05-27 15:13:27 -0700

    js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// Dropdown menu
 document.addEventListener('DOMContentLoaded', function() {
-  var dropdowns = document.querySelectorAll('.dropdown');
-  for (var i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].addEventListener('click', function() {
-      this.classList.toggle('active');
-      var dropdownContent = this.querySelector('.dropdown-content');
-      dropdownContent.classList.toggle('show');
+    var dropdowns = document.querySelectorAll('.dropdown');
+    
+    // Toggle dropdown menu
+    dropdowns.forEach(function(dropdown) {
+      dropdown.addEventListener('click', function() {
+        this.classList.toggle('active');
+        var dropdownContent = this.querySelector('.dropdown-content');
+        dropdownContent.classList.toggle('show');
+      });
     });
-  }
-});
-
-// Interactive buttons
-var buttons = document.querySelectorAll('.cta-button');
-for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function() {
-    alert('Button clicked!');
+    
+    // Interactive buttons
+    var buttons = document.querySelectorAll('.cta-button');
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function(event) {
+        event.preventDefault();
+        alert('Button clicked!');
+      });
+    });
   });
-}
